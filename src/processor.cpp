fcb1ef1d9624e0de4e80ce6bbac040a0611c4c64
Dan Keefe <peritract@hotmail.co.uk>
2020-06-29 00:15:56 +0100

    Update processor.cpp

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,9 @@
 #include "processor.h"
+#include "linux_parser.h"
 
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+using std::stof;
+
+// DONE: Return the aggregate CPU utilization
+float Processor::Utilization() {
+  return (float)LinuxParser::ActiveJiffies() / (float)LinuxParser::Jiffies();
+}
