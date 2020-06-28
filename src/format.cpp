8df11931b285d7c91fae4dae7faf4f6ac29ec45f
Dan Keefe <peritract@hotmail.co.uk>
2020-06-29 00:13:12 +0100

    Format

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,45 @@
 #include <string>
+#include <sstream>
 
 #include "format.h"
 
 using std::string;
+using std::to_string;
 
-// TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) {
+  string temp;
+  // Create the string stream
+  std::ostringstream time;
+  
+  // Push the hours into the stream
+  temp = to_string(seconds / 3600);
+  if (temp.size() == 1){
+  	temp = "0" + temp;
+  }
+  time << temp << ":";
+  
+  // Chunk off the hours
+  seconds = seconds % 3600;
+  
+  // Push the minutes in
+  temp = to_string(seconds / 60);
+  if (temp.size() == 1){
+  	temp = "0" + temp;
+  }
+  time << temp << ":";
+  
+  // Chunk off the minutes
+  seconds = seconds % 60;
+  
+  // Add the seconds to the stream
+  temp = to_string(seconds);
+  if (temp.size() == 1){
+  	temp = "0" + temp;
+  }
+  time << temp;
+  
+  // Return the final value as a string
+  return time.str();
+}
